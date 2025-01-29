200232c4081f8432a1db309c1b1452e3cd97e78b
DESKTOP-EAI04DP\mkana <m.kanamaru.it@gmail.com>
2025-01-29 15:54:22 -0800

    SQL driver test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include "ToDoList.h"
 #include <QtWidgets/QApplication>
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+#include <QtSql/QSqlError>
+#include <QDebug>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
+
+    if (QSqlDatabase::isDriverAvailable("QSQLITE")) {
+        qDebug() << "SQLite driver is available";
+
+        QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+        db.setDatabaseName("todo.db");
+        if (!db.open()) {
+            qDebug() << "Failed to open database:" << db.lastError().text();
+            return 1;
+        }
+    }
+    else {
+        qDebug() << "SQLite driver is not available";
+        return 1;
+    }
+
     ToDoList w;
     w.show();
     return a.exec();
